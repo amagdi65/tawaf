060e8b14ee84314001cdd046295e5b311573f258
ahmed magdy <ahmed.magdy@fixedmea.com>
2024-11-25 14:24:14 +0300

    commit from bash script

--- src/data/file_2.js
+++ src/data/file_2.js
@@ -3,7 +3,7 @@ export const data = [
     "ar": "فإذا أتممت الطواف سبعة أشواط فغطِ كتفيك برداء الإحرام، و توجه إلى مقام إبراهيم\r\n واقرأ : قوله تعالى( وَاتَّخِذُوا مِنْ مَقَامِ إِبْرَاهِيمَ مُصَلّىً)",
     "audioNumber": null,
     "en": "After completing the seven rounds of Tawaf , cover your shoulders with ihram, and head to the station of Ibrahim and recite: ((Take the place where Ibrahim stood as your place of prayer.))",
-    "ur": "جب آپ کا سات چکر طواف مکمل ہوجائے، تو احرام کي چادر سے اپنے کندھے کو ڈھانپ ليں اور مقام ابراہيم کي طرف رخ کريں ، اور اس آيت کي تلاوت کريں: (وَاتَّخِذُوا مِنْ مَقَامِ إِبْرَاهِيمَ مُصَلّىً )",
+    "ur": "جب آپ کا سات چکر طواف مکمل ہوجائے، تو احرام کی چادر سے اپنے کندھے کو ڈھانپ لیں اور مقام ابراہیم کی طرف رخ کریں ، اور اس آیت کی تلاوت کریں: (وَاتَّخِذُوا مِنْ مَقَامِ إِبْرَاهِيمَ مُصَلّىً )",
     "tr": "Tavafın yedi şavtını tamamladığınızda, ihram örtüsüyle açtığınız sağ omuzunu geri örtün ve Makâm-ı İbrahim’e varıp şu âyeti okuyun: \"Siz de Makâm-ı İbrahim’i bir namaz yeri edinin\"",
     "fr": "Après avoir accompli sept tours autour de la Ka'ba, recouvrez vos épaules des vêtements de l'ihram et dirigez-vous vers la station d'Ibrahim et récitez: \"Prenez donc la station d'Abraham pour lieu de prière.\" ",
     "in": "\"Apabila anda telah menyelesaikan tawaf tujuh putaran, maka tutuplah pundak anda dengan kain ihram, lalu pergilah ke Maqam Ibrahim, sembari membaca, \n\"\"Wattakhidzuu min maqaami Ibrahima mushalla\"\"\n(Dan jadikanlah maqam Ibrahim itu sebagai tempat shalat)\"",
@@ -19,10 +19,10 @@ export const data = [
     }
   },
   {
-    "ar": "\"ومن السنّة ان تصلي ركعتين خلف المقام ، واجعل المقام بينك وبين الكعبة المشرفة \nإن تيسر فهذه هي السنة، وإن لم يتيسر لك ذلك لزحام أو غيره فصلهما في أي موضع من المسجد الحرام، ويستحب أن تقرأ في الركعة الأولى بعد الفاتحة: ? قل يا أيها الكافرون ?، وفي الأخرى بعد الفاتحة : ? قل هو الله أحد ?\" ",
+    "ar": "\"ومن السنّة ان تصلي ركعتين خلف المقام ، واجعل المقام بينك وبين الكعبة المشرفة \r\nإن تيسر فهذه هي السنة، وإن لم يتيسر لك ذلك لزحام أو غيره فصلهما في أي موضع من المسجد الحرام، ويستحب أن تقرأ في الركعة الأولى بعد الفاتحة: ﴿ قل يا أيها الكافرون ﴾، وفي الأخرى بعد الفاتحة : ﴿ قل هو الله أحد ﴾\" ",
     "audioNumber": null,
     "en": "Then pray two Rak'ats behind the station of Ibrahim if you are able to do so, or pray at any other place inside the Masjid. You should recite in the first Rak'ah after al-Fatihah: {Qul ya ayyuhal kafirun} and in the second Rak'ah after al-Fatiha: {Qul Huwallahu Ahad}",
-    "ur": "\"تم مقام ابراہيم کو جائے نماز مقرر کر لو\" پھر اگر ممکن ہو تو مقام ابراہيم کے پيچھے دو رکعت نماز ادا کريں، ورنہ مسجد کے کسي حصہ ميں بھي? پہلي رکعت ميں فاتحہ کے بعد سورہ : {قل يا أيها الكافرون} ، اور دوسري رکعت ميں فاتحہ کے بعد : { قل هو الله أحد}",
+    "ur": "\"تم مقام ابراہیم کو جائے نماز مقرر کر لو\" پھر اگر ممکن ہو تو مقام ابراہیم کے پیچھے دو رکعت نماز ادا کریں، ورنہ مسجد کے کسی حصہ میں بھی۔ پہلی رکعت میں فاتحہ کے بعد سورہ : {قل يا أيها الكافرون} ، اور دوسری رکعت میں فاتحہ کے بعد : { قل هو الله أحد}",
     "tr": " Makâm-ı İbrahim'in arkasında iki rekât namaz kılmanız sünnettir. Eğer mümkünse, Makâm-ı İbrahim’i kendiniz ile Kâbe-i Müşerrefe arasına alınız. Sünnete uygun olan budur. Eğer yoğunluktan veya başka bir sebepten dolayı mümkün değilse, bu iki rekât namazı Mescid-i Haram’ın herhangi bir yerinde kılabilirsiniz. Birinci rekâtta Fatiha'dan sonra Kâfirûn Suresi ve ikinci rekâtta Fatiha'dan sonra İhlâs Suresini okumanız sünnettir.",
     "fr": "Puis, accomplissez deux rak'a derrière la station si possible, sinon n'importe où dans la Mosquée. Dans la première rak'a, après Al-Fatiha, récitez la sourate Al-Kafiroune et dans la deuxième rak'a, après Al-Fatiha, récitez la sourate Al-Ikhlas",
     "in": "Disunnahkan untuk mengerjakan shalat dua rakaat di belakang maqam Ibrahim. Jadikanlah maqam Ibrahim berposisi di antara anda dan Ka'bah yang mulia. Jika ini mudah, maka itu adalah sunnah. Tetapi jika hal itu tidak mudah karena kepadatan dan lainnya, maka anda dapat shalat dua rakaat di area manapun di Masjidil Haram. Disunnahkan pada rakaat pertama setelah membaca al-Fatihah untuk membaca surat al-Kafirun (Qul yaa ayyuhal kafirun..). Adapun pada rakaat kedua, setelah al-Fatihah membaca surat al-Ikhlas (Qul huwallahu ahad..).",
