6593e96109991f5d912f9d822173137918a980b0
Fahad Egab <Fahadegabb@gmail.com>
2025-03-26 19:06:47 +0300

    in lang update

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -43,8 +43,7 @@ const textContent = {
     header: "Votre Assistant pour le Voyage de la Omra",
     description: `Durant le voyage de la Omra, une assistance virtuelle de plus de 170 invocations écrites et audios vous accompagne tout au long du Tawaf et du Sa’i.`,
     confirmLabel: "Commencez la Omra",
-    conditionsStatment: `Chers hôtes du Tout Miséricordieux,
-Pour votre confort et afin de faciliter l’accomplissement de vos rites, nous vous prions de bien respecter les directives suivantes :`,
+    conditionsStatment: `Chers hôtes du Tout Miséricordieux, Pour votre confort et afin de faciliter l’accomplissement de vos rites, nous vous prions de bien respecter les directives suivantes :`,
     firstConds: `Obtenez votre permis de la Omra via l’application Nusuk avant de vous rendre à la Mosquée Sacrée.`,
     secndConds: `Utilisez des écouteurs pour écouter les invocations, afin de préserver la sérénité des lieux pour les fidèles et les pèlerins.`,
     thirdConds: `Pour profiter pleinement du contenu de la plateforme, nous vous recommandons de ne pas utiliser d'applications de traduction d'écran.`,
@@ -54,8 +53,7 @@ Pour votre confort et afin de faciliter l’accomplissement de vos rites, nous v
     header: "Umre Yolculuğunuzda Yardımcınız",
     description: `Tavâf ve Sa’y esnasında yazılı ve işitsel 170'ten fazla dua ile umre yolculuğunda sizin yardımcınız olmaktadır.`,
     confirmLabel: "Umreye Başla",
-    conditionsStatment: `Rahman olan Allah’ın Kıymetli Misafirleri,
-Rahatınızı ve ibadetlerinizi kolaylıkla yerine getirebilmenizi sağlamak için lütfen aşağıdaki kurallara uyunuz:`,
+    conditionsStatment: `Rahman olan Allah’ın Kıymetli Misafirleri, Rahatınızı ve ibadetlerinizi kolaylıkla yerine getirebilmenizi sağlamak için lütfen aşağıdaki kurallara uyunuz:`,
     firstConds: `Mescid-i Haram’a gitmeden önce umre izninizi “Nusuk” uygulaması üzerinden çıkartınız.`,
     secndConds: `Cemaatin ve umrecilerin huzurunu korumak adına duaları dinlerken kulaklık kullanınız. `,
     thirdConds: `Hizmet hakkında sunulan içeriğin kalitesini korumak için göz gezdirirken ekran çeviri programlarını kullanmaktan kaçınınız.`,
@@ -66,30 +64,29 @@ Rahatınızı ve ibadetlerinizi kolaylıkla yerine getirebilmenizi sağlamak iç
     description:
       " سفر عمرہ میں آپ کا معاون، طواف و سعی کے دوران ٹیکسٹ اور آڈیو کی شکل میں 170 سے زیادہ دعاؤں کے ساتھ .",
     confirmLabel: "عمرہ شروع کریں",
-    conditionsStatment: `رحمن کے مہمانو!
- آپ اپنی راحت اور عبادت کی ادائیگی میں سہولت کے پیش نظرہم امید کرتے ہیں کہ درج ذیل تعلمیات کی پابندی کریں:`,
+    conditionsStatment: `رحمن کے مہمانو! آپ اپنی راحت اور عبادت کی ادائیگی میں سہولت کے پیش نظرہم امید کرتے ہیں کہ درج ذیل تعلمیات کی پابندی کریں:`,
     firstConds: `مسجد حرام جانے سے پہلے "نسک" ایپ کے ذریعہ عمرہ کی  تصریح (پرمیشن) نکال لیں۔`,
     secndConds: `دعاؤں کو سنتے وقت ایئرفون کا استعمال کریں؛ تاکہ دیگر نمازیوں اور عمرہ کرنے والوں کی راحت کا خیال رکھا جايے۔`,
     thirdConds: `خدمت کو براؤز کرتے وقت مواد کے معیاری ڈسپلے کو یقینی بنانے کے لیے اسکرین ٹرانسلیشن پروگراموں کے استعمال سے گریز کریں۔`,
     consclusion: `ہم اللہ سے آپ کے لیے مقبول عمرہ اور قابل قدر کوشش کی دعا کرتے ہیں۔`,
   },
   in: {
-    header: "Bahasa Indonesia Assistant",
+    header: "Panduan Anda dalam Perjalanan Ibadah Umrah",
     description: `Membantu anda dalam perjalanan umrah dengan himpunan lebih dari 170 doa, baik teks maupun audio saat melakukan tawaf dan sai.`,
     confirmLabel: "Silakan memulai ibadah umrah",
-    conditionsStatment: `Para tamu Allah yang terhormat
-Demi kenyamanan dan kemudahan dalam melaksanakan ibadah anda, kami mohon untuk mengikuti petunjuk-petunjuk berikut:`,
+    conditionsStatment: `Para tamu Allah yang terhormat 
+    Demi kenyamanan dan kemudahan dalam melaksanakan ibadah anda, kami mohon untuk mengikuti petunjuk-petunjuk berikut:`,
     firstConds: `Menerbitkan izin umrah melalui aplikasi "Nusuk" sebelum menuju Masjidil Haram.`,
     secndConds: `Menggunakan earphone saat mendengarkan doa-doa, demi kenyamanan jamaah shalat dan umrah.`,
     thirdConds: `Hindari penggunaan aplikasi penerjemah layar untuk memastikan kualitas tampilan konten saat mengakses layanan ini.`,
-    consclusion: `Kami memohon kepada Allah agar menerima umrah anda dan memberikan pahala yang besar.`,
+    consclusion: `Kami memohon kepada Allah 
+    agar menerima umrah anda dan memberikan pahala yang besar.`,
   },
   mal: {
     header: "Bahasa Melayu Assistant",
     description: `Panduan Perjalanan Umrah Anda semasa tawaf dan sa'i yang dilengkapi dengan lebih 170 jenis doa berbentuk teks dan audio.`,
     confirmLabel: "Sila mulakan ibadat umrah anda",
-    conditionsStatment: `Para Dhuyufurrahman,
-bagi memastikan pelaksanaan ibadah umrah anda berjalan dengan mudah dan lancar, anda boleh:`,
+    conditionsStatment: `Para Dhuyufurrahman, bagi memastikan pelaksanaan ibadah umrah anda berjalan dengan mudah dan lancar, anda boleh:`,
     firstConds: `Memohon permit umrah melalui aplikasi "Nusuk" sebelum anda menuju ke Masjidil Haram.`,
     secndConds: `Anda juga boleh menggunakan fon telinga (earphone) untuk mendengar pelbagai jenis doa, bagi memastikan keselesaan para jemaah yang lain.`,
     thirdConds: `Elakkan daripada menggunakan aplikasi terjemahan skrin bagi memastikan kualiti kandungan perkhidmatan semasa anda menggunakan perkhidmatan ini.`,
@@ -234,7 +231,7 @@ const Popup = ({ setLang, lang, setDir, dir: pageDir }) => {
               color="gray.800"
               px={4}
               lineHeight="1.5"
-              style={{ direction: pageDir }}
+              style={{ direction: pageDir, whiteSpace: "pre-line" }}
             >
               {textContent[lang].conditionsStatment}
             </Text>
@@ -264,7 +261,7 @@ const Popup = ({ setLang, lang, setDir, dir: pageDir }) => {
               color="gray.600"
               px={4}
               lineHeight="1.5"
-              style={{ direction: pageDir }}
+              style={{ direction: pageDir, whiteSpace: "pre-line" }}
             >
               {textContent[lang].consclusion}
             </Text>
